42c034f6dc959534fae40ef92999a705c65b089d
HouYong Yang <240011814@qq.com>
2026-05-11 14:27:37 +0800

    feat(cut): 修复cut_record表中user_id字段的数据类型

--- backend/service/db/migrations/20260511000004_fix_cut_record_user_id_type.sql
+++ backend/service/db/migrations/20260511000004_fix_cut_record_user_id_type.sql
@@ -1,8 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE `cut_record` MODIFY COLUMN `user_id` BIGINT UNSIGNED NOT NULL;
+-- +goose StatementEnd
 
--- 修复可能存在的字符串类型数据
+-- +goose StatementBegin
 UPDATE `cut_record` SET `user_id` = CAST(`user_id` AS UNSIGNED) WHERE `user_id` REGEXP '^[0-9]+$';
 -- +goose StatementEnd
 
