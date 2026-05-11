6f6e98d0e22a2ae440cd1a71285d331cf6d948f0
HouYong Yang <240011814@qq.com>
2026-05-11 09:24:37 +0800

    feat: 修改自定义训练ID的添加方式，调整索引创建和删除顺序

--- backend/service/db/migrations/20240509000014_add_custom_training_id_to_histories.sql
+++ backend/service/db/migrations/20240509000014_add_custom_training_id_to_histories.sql
@@ -2,12 +2,14 @@
 -- SQL in section 'Up' is executed when this migration is applied
 
 ALTER TABLE training_histories
-ADD COLUMN custom_training_id INT UNSIGNED NULL COMMENT '自定义训练ID，仅自定义训练类型使用' AFTER training_type,
-ADD INDEX idx_custom_training_id (custom_training_id);
+ADD COLUMN custom_training_id INT UNSIGNED NULL COMMENT '自定义训练ID，仅自定义训练类型使用';
+
+CREATE INDEX idx_custom_training_id ON training_histories(custom_training_id);
 
 -- +goose Down
 -- SQL in section 'Down' is executed when this migration is rolled back
 
+DROP INDEX idx_custom_training_id ON training_histories;
+
 ALTER TABLE training_histories
-DROP INDEX idx_custom_training_id,
 DROP COLUMN custom_training_id;
